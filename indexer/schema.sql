abf68374523a68201651367f57a8850a6cd4e124
Kijin Sung <kijin@kijinsung.com>
2014-01-18 15:00:05 +0900

    Add comments to DB schema

--- indexer/schema.sql
+++ indexer/schema.sql
@@ -1,4 +1,6 @@
 
+-- 이미 존재하는 테이블과 프로시저를 삭제한다.
+
 DROP TABLE IF EXISTS postcode_addresses;
 DROP TABLE IF EXISTS postcode_keywords_juso;
 DROP TABLE IF EXISTS postcode_keywords_jibeon;
@@ -17,68 +19,86 @@ DROP PROCEDURE IF EXISTS postcode_search_building_with_dongri_in_area;
 DROP PROCEDURE IF EXISTS postcode_search_pobox;
 DROP PROCEDURE IF EXISTS postcode_search_pobox_in_area;
 
+-- 주소 정보를 저장하는 메인 테이블.
+
 CREATE TABLE postcode_addresses (
-    id NUMERIC(25) PRIMARY KEY,
-    postcode5 CHAR(5),
-    postcode6 CHAR(6),
-    road_id NUMERIC(12),
-    road_section CHAR(2),
-    road_name VARCHAR(80),
-    num_major SMALLINT(5) UNSIGNED,
-    num_minor SMALLINT(5) UNSIGNED,
-    is_basement TINYINT(1) DEFAULT 0,
-    sido VARCHAR(20),
-    sigungu VARCHAR(20),
-    ilbangu VARCHAR(20),
-    eupmyeon VARCHAR(20),
-    dongri VARCHAR(20),
-    jibeon VARCHAR(10),
-    building_name VARCHAR(40),
-    other_addresses VARCHAR(600)
+    id NUMERIC(25) PRIMARY KEY,                         -- 관리번호 (PK)
+    postcode5 CHAR(5),                                  -- 기초구역번호
+    postcode6 CHAR(6),                                  -- 우편번호
+    road_id NUMERIC(12),                                -- 도로번호
+    road_section CHAR(2),                               -- 도로구간번호
+    road_name VARCHAR(80),                              -- 도로명
+    num_major SMALLINT(5) UNSIGNED,                     -- 도로명주소 주번호
+    num_minor SMALLINT(5) UNSIGNED,                     -- 도로명주소 부번호
+    is_basement TINYINT(1) DEFAULT 0,                   -- 지하여부
+    sido VARCHAR(20),                                   -- 시/도
+    sigungu VARCHAR(20),                                -- 시/군/자치구
+    ilbangu VARCHAR(20),                                -- 일반구
+    eupmyeon VARCHAR(20),                               -- 읍/면
+    dongri VARCHAR(20),                                 -- 동/리
+    jibeon VARCHAR(10),                                 -- 지번
+    building_name VARCHAR(40),                          -- 공동주택명
+    other_addresses VARCHAR(600)                        -- 관련주소목록
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+-- 도로명주소 검색을 위한 키워드 테이블.
 
 CREATE TABLE postcode_keywords_juso (
-    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    address_id NUMERIC(25) NOT NULL,
-    keyword_crc32 INT(10) UNSIGNED,
-    num_major SMALLINT(5) UNSIGNED,
-    num_minor SMALLINT(5) UNSIGNED
+    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,        -- PK
+    address_id NUMERIC(25) NOT NULL,                    -- 관리번호 (FK)
+    keyword_crc32 INT(10) UNSIGNED,                     -- 도로명의 CRC32 값
+    num_major SMALLINT(5) UNSIGNED,                     -- 도로명주소 주번호
+    num_minor SMALLINT(5) UNSIGNED                      -- 도로명주소 부번호
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+-- 지번 검색을 위한 키워드 테이블.
 
 CREATE TABLE postcode_keywords_jibeon (
-    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    address_id NUMERIC(25) NOT NULL,
-    keyword_crc32 INT(10) UNSIGNED,
-    num_major SMALLINT(5) UNSIGNED,
-    num_minor SMALLINT(5) UNSIGNED
+    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,        -- PK
+    address_id NUMERIC(25) NOT NULL,                    -- 관리번호 (FK)
+    keyword_crc32 INT(10) UNSIGNED,                     -- 동/리명의 CRC32 값
+    num_major SMALLINT(5) UNSIGNED,                     -- 지번 주번호
+    num_minor SMALLINT(5) UNSIGNED                      -- 지번 부번호
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+-- 건물명 검색을 위한 키워드 테이블.
 
 CREATE TABLE postcode_keywords_building (
-    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    address_id NUMERIC(25) NOT NULL,
-    keyword VARCHAR(40),
-    admin_dongri INT(10) UNSIGNED,
-    legal_dongri INT(10) UNSIGNED
+    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,        -- PK
+    address_id NUMERIC(25) NOT NULL,                    -- 관리번호 (FK)
+    keyword VARCHAR(40),                                -- 건물명
+    admin_dongri INT(10) UNSIGNED,                      -- 행정동명의 CRC32 값
+    legal_dongri INT(10) UNSIGNED                       -- 법정동/리명의 CRC32 값
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+-- 사서함 검색을 위한 키워드 테이블.
 
 CREATE TABLE postcode_keywords_pobox (
-    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    address_id NUMERIC(25) NOT NULL,
-    keyword VARCHAR(40),
-    range_start_major SMALLINT(5) UNSIGNED,
-    range_start_minor SMALLINT(5) UNSIGNED,
-    range_end_major SMALLINT(5) UNSIGNED,
-    range_end_minor SMALLINT(5) UNSIGNED
+    seq INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,        -- PK
+    address_id NUMERIC(25) NOT NULL,                    -- 관리번호 (FK)
+    keyword VARCHAR(40),                                -- 사서함명 검색어
+    range_start_major SMALLINT(5) UNSIGNED,             -- 시작 주번호
+    range_start_minor SMALLINT(5) UNSIGNED,             -- 시작 부번호
+    range_end_major SMALLINT(5) UNSIGNED,               -- 끝 주번호
+    range_end_minor SMALLINT(5) UNSIGNED                -- 끝 부번호
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
 
+-- 각종 설정을 저장하는 테이블.
+
 CREATE TABLE postcode_metadata (
-    k VARCHAR(20) PRIMARY KEY,
-    v VARCHAR(40)
+    k VARCHAR(20) PRIMARY KEY,                          -- 설정 키
+    v VARCHAR(40)                                       -- 설정 값
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci;
+
+-- 기본 설정을 삽입한다.
+
 INSERT INTO postcode_metadata (k, v) VALUES ('version', '4.1');
 INSERT INTO postcode_metadata (k, v) VALUES ('updated', '00000000');
 
-CREATE PROCEDURE postcode_search_juso(IN keyword_crc32 INT UNSIGNED, IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED)
+-- 도로명주소 검색 (단순) 프로시저.
+
+CREATE PROCEDURE postcode_search_juso(IN keyword_crc32 INT UNSIGNED,
+    IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED)
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_juso AS pk ON pa.id = pk.address_id
@@ -89,8 +109,12 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_juso_in_area(IN keyword_crc32 INT UNSIGNED, IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED,
-    IN area1 VARCHAR(20), IN area2 VARCHAR(20), IN area3 VARCHAR(20), IN area4 VARCHAR(20))
+-- 도로명주소 검색 (지역 제한) 프로시저.
+
+CREATE PROCEDURE postcode_search_juso_in_area(IN keyword_crc32 INT UNSIGNED,
+    IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED,
+    IN area1 VARCHAR(20), IN area2 VARCHAR(20),
+    IN area3 VARCHAR(20), IN area4 VARCHAR(20))
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_juso AS pk ON pa.id = pk.address_id
@@ -105,7 +129,10 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_jibeon(IN keyword_crc32 INT UNSIGNED, IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED)
+-- 지번 검색 (단순) 프로시저.
+
+CREATE PROCEDURE postcode_search_jibeon(IN keyword_crc32 INT UNSIGNED,
+    IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED)
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_jibeon AS pk ON pa.id = pk.address_id
@@ -116,8 +143,12 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_jibeon_in_area(IN keyword_crc32 INT UNSIGNED, IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED,
-    IN area1 VARCHAR(20), IN area2 VARCHAR(20), IN area3 VARCHAR(20), IN area4 VARCHAR(20))
+-- 지번 검색 (지역 제한) 프로시저.
+
+CREATE PROCEDURE postcode_search_jibeon_in_area(IN keyword_crc32 INT UNSIGNED,
+    IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED,
+    IN area1 VARCHAR(20), IN area2 VARCHAR(20),
+    IN area3 VARCHAR(20), IN area4 VARCHAR(20))
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_jibeon AS pk ON pa.id = pk.address_id
@@ -131,6 +162,8 @@ BEGIN
     ORDER BY pa.sido, pa.sigungu, pa.road_name, pa.num_major, pa.num_minor
     LIMIT 100;
 END;
+
+-- 건물명 검색 (단순) 프로시저.
 
 CREATE PROCEDURE postcode_search_building(IN keyword VARCHAR(80))
 BEGIN
@@ -141,8 +174,11 @@ BEGIN
     LIMIT 100;
 END;
 
+-- 건물명 검색 (지역 제한) 프로시저.
+
 CREATE PROCEDURE postcode_search_building_in_area(IN keyword VARCHAR(80),
-    IN area1 VARCHAR(20), IN area2 VARCHAR(20), IN area3 VARCHAR(20), IN area4 VARCHAR(20))
+    IN area1 VARCHAR(20), IN area2 VARCHAR(20),
+    IN area3 VARCHAR(20), IN area4 VARCHAR(20))
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_building AS pk ON pa.id = pk.address_id
@@ -155,7 +191,10 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_building_with_dongri(IN keyword VARCHAR(80), IN dongri_crc32 INT UNSIGNED)
+-- 건물명 + 동/리 검색 (단순) 프로시저.
+
+CREATE PROCEDURE postcode_search_building_with_dongri(IN keyword VARCHAR(80),
+    IN dongri_crc32 INT UNSIGNED)
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_building AS pk ON pa.id = pk.address_id
@@ -165,8 +204,12 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_building_with_dongri_in_area(IN keyword VARCHAR(80), IN dongri_crc32 INT UNSIGNED,
-    IN area1 VARCHAR(20), IN area2 VARCHAR(20), IN area3 VARCHAR(20), IN area4 VARCHAR(20))
+-- 건물명 + 동/리 검색 (지역 제한) 프로시저.
+
+CREATE PROCEDURE postcode_search_building_with_dongri_in_area(IN keyword VARCHAR(80),
+    IN dongri_crc32 INT UNSIGNED,
+    IN area1 VARCHAR(20), IN area2 VARCHAR(20),
+    IN area3 VARCHAR(20), IN area4 VARCHAR(20))
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_building AS pk ON pa.id = pk.address_id
@@ -180,7 +223,10 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_pobox(IN keyword VARCHAR(80), IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED)
+-- 사서함 검색 (단순) 프로시저.
+
+CREATE PROCEDURE postcode_search_pobox(IN keyword VARCHAR(80),
+    IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED)
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_pobox AS pk ON pa.id = pk.address_id
@@ -191,8 +237,12 @@ BEGIN
     LIMIT 100;
 END;
 
-CREATE PROCEDURE postcode_search_pobox_in_area(IN keyword VARCHAR(80), IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED,
-    IN area1 VARCHAR(20), IN area2 VARCHAR(20), IN area3 VARCHAR(20), IN area4 VARCHAR(20))
+-- 사서함 검색 (지역 제한) 프로시저.
+
+CREATE PROCEDURE postcode_search_pobox_in_area(IN keyword VARCHAR(80),
+    IN num1 SMALLINT UNSIGNED, IN num2 SMALLINT UNSIGNED,
+    IN area1 VARCHAR(20), IN area2 VARCHAR(20),
+    IN area3 VARCHAR(20), IN area4 VARCHAR(20))
 BEGIN
     SELECT DISTINCT pa.* FROM postcode_addresses AS pa
     INNER JOIN postcode_keywords_pobox AS pk ON pa.id = pk.address_id
