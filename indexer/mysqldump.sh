3104c3336cfcf08c9583241a0971912d1f04aa35
Kijin Sung <kijin@kijinsung.com>
2014-01-19 00:05:21 +0900

    Add better options to the mysqldump script

--- indexer/mysqldump.sh
+++ indexer/mysqldump.sh
@@ -5,6 +5,9 @@ MYSQL_USER="me"
 MYSQL_PASS="me"
 MYSQL_DBNAME="me_postcode"
 
-mysqldump --opt --routines --single_transaction \
+mysqldump --opt --order-by-primary --routines \
+    --single_transaction --no-autocommit --skip-tz-utc \
     -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS \
-    $MYSQL_DBNAME | gzip > dump.sql.gz
+    $MYSQL_DBNAME | sed 's/DEFINER=`.*`@`.*` //' \
+    | gzip > dump.sql.gz
+
