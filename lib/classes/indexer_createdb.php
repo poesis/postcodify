acdc71608cd0cefe82ef17a8e3f08eab9c672c31
Kijin Sung <kijin@kijinsung.com>
2014-09-30 20:13:42 +0900

    Add detailed comments

--- lib/classes/indexer_createdb.php
+++ lib/classes/indexer_createdb.php
@@ -27,6 +27,8 @@ class Postcodify_Indexer_CreateDB
     protected $_data_date;
     protected $_shmop_key;
     
+    // 쓰레드별로 작업을 분할하는 데 사용하는 시도 목록.
+    
     protected $_thread_groups = array(
         '경기도', '경상남북도', '전라남북도', '충청남북도',
         '서울특별시|부산광역시|세종특별자치시|제주특별자치도',
@@ -73,13 +75,33 @@ class Postcodify_Indexer_CreateDB
         $this->print_ok();
         $this->print_newline();
         
-        $this->print_message('주소 파일을 로딩하는 중...');
+        $this->print_message('주소 데이터를 로딩하는 중...');
         $this->start_threaded_workers('juso');
         $this->print_ok();
         $this->print_newline();
         
         $this->print_message('작업용 인덱스를 생성하는 중...');
         $this->create_interim_indexes();
+        $this->print_ok();
+        $this->print_newline();
+        
+        $this->print_message('지번 데이터를 로딩하는 중...');
+        $this->start_threaded_workers('jibeon');
+        $this->print_ok();
+        $this->print_newline();
+        
+        $this->print_message('부가정보 데이터를 로딩하는 중...');
+        $this->start_threaded_workers('extra_info');
+        $this->print_ok();
+        $this->print_newline();
+        
+        $this->print_message('사서함 데이터를 로딩하는 중...');
+        $this->load_pobox();
+        $this->print_ok();
+        $this->print_newline();
+        
+        $this->print_message('최종 인덱스를 생성하는 중...');
+        $this->create_final_indexes();
         $this->print_ok();
         $this->print_newline();
     }
@@ -118,11 +140,17 @@ class Postcodify_Indexer_CreateDB
     
     public function start_threaded_workers($task)
     {
+        // 카운터로 사용하는 공유 메모리를 초기화한다.
+        
         $shmop = shmop_open($this->_shmop_key, 'c', 0644, 4);
         shmop_write($shmop, pack('L', 0), 0);
         
+        // 자식 프로세스 목록을 초기화한다.
+        
         $children = array();
         $sidos = $this->_thread_groups;
+        
+        // 자식 프로세스들을 생성한다.
         
         while (count($sidos))
         {
@@ -146,14 +174,26 @@ class Postcodify_Indexer_CreateDB
             }
         }
         
+        // 자식 프로세스들이 작업을 마치기를 기다린다.
+        
         while (count($children))
         {
+            // 작업을 마친 자식 프로세스는 목록에서 삭제한다.
+            
             $pid = pcntl_wait($status, WNOHANG | WUNTRACED);
             if ($pid) unset($children[$pid]);
+            
+            // 카운터를 확인한다.
+            
             $count = current(unpack('L', shmop_read($shmop, 0, 4)));
             $this->print_progress($count);
+            
+            // 부모 프로세스는 쉰다.
+            
             usleep(100000);
         }
+        
+        // 공유 메모리를 닫는다.
         
         shmop_close($shmop);
     }
@@ -197,8 +237,12 @@ class Postcodify_Indexer_CreateDB
     
     public function load_data_date()
     {
+        // 파일에서 데이터 기준일을 읽는다.
+        
         $date = trim(file_get_contents($this->_data_dir . '/도로명코드_기준일.txt'));
         $this->_data_date = $date;
+        
+        // DB에 저장한다.
         
         if (!DRY_RUN)
         {
@@ -213,6 +257,8 @@ class Postcodify_Indexer_CreateDB
     
     public function load_road_info()
     {
+        // DB를 준비한다.
+        
         if (!DRY_RUN)
         {
             $db = Postcodify_Utility::get_db();
@@ -222,19 +268,29 @@ class Postcodify_Indexer_CreateDB
                 'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
         }
         
+        // Zip 파일을 연다.
+        
         $zip = new Postcodify_Indexer_Parser_Road_List;
         $zip->open_archive($this->_data_dir . '/도로명코드_전체분.zip');
         $zip->open_next_file();
         
+        // 카운터를 초기화한다.
+        
         $count = 0;
+        
+        // 데이터를 한 줄씩 읽는다.
         
         while ($entry = $zip->read_line())
         {
+            // 영문 행정구역명을 캐시에 저장한다.
+            
             Postcodify_Utility::$english_cache[$entry->road_name] = $entry->road_name_english;
             Postcodify_Utility::$english_cache[$entry->sido] = $entry->sido_english;
             if ($entry->sigungu) Postcodify_Utility::$english_cache[$entry->sigungu] = $entry->sigungu_english;
             if ($entry->ilbangu) Postcodify_Utility::$english_cache[$entry->ilbangu] = $entry->ilbangu_english;
             if ($entry->eupmyeon) Postcodify_Utility::$english_cache[$entry->eupmyeon] = $entry->eupmyeon_english;
+            
+            // 도로명 및 소속 행정구역 정보를 DB에 저장한다.
             
             if (!DRY_RUN)
             {
@@ -253,9 +309,13 @@ class Postcodify_Indexer_CreateDB
                 ));
             }
             
+            // 카운터를 표시한다.
+            
             if (++$count % 512 === 0) $this->print_progress($count);
             unset($entry);
         }
+        
+        // 뒷정리.
         
         $zip->close();
         unset($zip);
@@ -271,46 +331,79 @@ class Postcodify_Indexer_CreateDB
     
     public function load_building_info()
     {
+        // Zip 파일을 연다.
+        
         $zip = new Postcodify_Indexer_Parser_Building_Info;
         $zip->open_archive($this->_data_dir . '/상세건물명.zip');
         $zip->open_next_file();
         
+        // 카운터를 초기화한다.
+        
         $count = 0;
+        
+        // 데이터를 한 줄씩 읽는다.
         
         while ($entry = $zip->read_line())
         {
+            // 건물명을 캐시에 저장한다.
+            
             if (count($entry->building_names))
             {
                 Postcodify_Utility::$building_cache[$entry->address_id] = implode(',', $entry->building_names);
             }
             
+            // 카운터를 표시한다.
+            
             if (++$count % 512 === 0) $this->print_progress($count);
             unset($entry);
         }
+        
+        // 뒷정리.
+        
+        $zip->close();
+        unset($zip);
     }
     
     // 영문 행정구역명을 로딩한다.
     
     public function load_english_aliases()
     {
+        // Zip 파일을 연다.
+        
         $zip = new Postcodify_Indexer_Parser_English_Aliases;
         $zip->open_archive($this->_data_dir . '/english_aliases_DB.zip');
         $zip->open_next_file();
         
+        // 카운터를 초기화한다.
+        
         $count = 0;
+        
+        // 데이터를 한 줄씩 읽는다.
         
         while ($entry = $zip->read_line())
         {
+            // 영문 행정구역명을 캐시에 저장한다.
+            
             Postcodify_Utility::$english_cache[$entry->ko] = $entry->en;
+            
+            // 카운터를 표시한다.
+            
             if (++$count % 512 === 0) $this->print_progress($count);
             unset($entry);
         }
+        
+        // 뒷정리.
+        
+        $zip->close();
+        unset($zip);
     }
     
-    // 주소 파일을 로딩한다. (쓰레드 사용)
+    // 주소 데이터를 로딩한다. (쓰레드 사용)
     
     public function load_juso($sido)
     {
+        // DB를 준비한다.
+        
         if (!DRY_RUN)
         {
             $db = Postcodify_Utility::get_db();
@@ -318,19 +411,33 @@ class Postcodify_Indexer_CreateDB
             $ps = $db->prepare('INSERT INTO postcodify_addresses');
         }
         
+        // 이 쓰레드에서 처리할 시·도 목록을 구한다.
+        
         $sidos = explode('|', $sido);
+        
+        // 카운터를 초기화한다.
+        
+        $count = 0;
+        
+        // 시·도를 하나씩 처리한다.
         
         foreach ($sidos as $sido)
         {
+            // Zip 파일을 연다.
+            
             $zip = new Postcodify_Indexer_Parser_Juso;
             $zip->open_archive($this->_data_dir . '/주소_' . $sido . '.zip');
             
+            // Zip 파일에 포함된 텍스트 파일들을 하나씩 처리한다.
+            
             while (($filename = $zip->open_next_file()) !== false)
             {
-                $count = 0;
+                // 데이터를 한 줄씩 읽는다.
                 
                 while ($entry = $zip->read_line())
                 {
+                    // 카운터를 표시한다.
+                    
                     if (++$count % 512 === 0)
                     {
                         $shmop = shmop_open($this->_shmop_key, 'w', 0, 0);
@@ -338,10 +445,13 @@ class Postcodify_Indexer_CreateDB
                         shmop_write($shmop, pack('L', $prev + 512), 0);
                         shmop_close($shmop);
                     }
+                    
                     unset($entry);
                 }
             }
         }
+        
+        // 뒷정리.
         
         $zip->close();
         unset($zip);
@@ -353,28 +463,28 @@ class Postcodify_Indexer_CreateDB
         }
     }
     
-    // 지번 파일을 로딩한다. (쓰레드 사용)
+    // 지번 데이터를 로딩한다. (쓰레드 사용)
     
     public function load_jibeon($sido)
     {
         
     }
     
-    // 부가정보 파일을 로딩한다. (쓰레드 사용)
+    // 부가정보 데이터를 로딩한다. (쓰레드 사용)
     
     public function load_extra_info($sido)
     {
         
     }
     
-    // 사서함 파일을 로딩한다.
+    // 사서함 데이터를 로딩한다.
     
     public function load_pobox()
     {
         
     }
     
-    // 지번 우편번호 파일을 로딩한다.
+    // 지번 우편번호 데이터를 로딩한다.
     
     public function load_jibeon_postcode()
     {
