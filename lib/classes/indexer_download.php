2e6e0721b30aec11b214954e4ac47ed3e08e3c5c
Kijin Sung <kijin@kijinsung.com>
2015-03-04 18:39:49 +0900

    Add oldaddr_zipcode_DB.zip to list of files to download

--- lib/classes/indexer_download.php
+++ lib/classes/indexer_download.php
@@ -26,8 +26,8 @@ class Postcodify_Indexer_Download
     const RELATIVE_DOMAIN = 'http://www.juso.go.kr';
     const LIST_URL = '/notice/OpenArchivesList.do?currentPage=1&countPerPage=20&noticeKd=26&type=matching';
     const POBOX_URL = 'http://www.epost.go.kr/search/zipcode/newaddr_pobox_DB.zip';
-    const JIBEON_URL = 'http://www.epost.go.kr/search/zipcode/koreapost_zipcode_DB.zip';
     const ENGLISH_URL = 'http://cdn.poesis.kr/archives/english_aliases_DB.zip';
+    const OLDADDR_URL = 'http://cdn.poesis.kr/archives/oldaddr_zipcode_DB.zip';
     const POBOX_NEWCODE_URL = 'http://cdn.poesis.kr/archives/newcode_pobox_DB.zip';
     const FIND_ENTRIES_REGEXP = '#<td class="align-left">(.+)</td>#isU';
     const FIND_LINKS_IN_ENTRY_REGEXP = '#<a href="([^"]+)">#iU';
@@ -197,6 +197,22 @@ class Postcodify_Indexer_Download
             $downloaded_files++;
         }
         
+        // 지번주소 기준 우편번호 데이터를 다운로드한다.
+        
+        Postcodify_Utility::print_message('다운로드: ' . basename(self::OLDADDR_URL));
+        $filepath = $download_path . '/' . basename(self::OLDADDR_URL);
+        $result = Postcodify_Utility::download(self::OLDADDR_URL, $filepath);
+        if (!$result || !file_exists($filepath) || filesize($filepath) < 1024)
+        {
+            Postcodify_Utility::print_error();
+            exit(2);
+        }
+        else
+        {
+            Postcodify_Utility::print_ok();
+            $downloaded_files++;
+        }
+        
         // 영문 동 명칭을 다운로드한다.
         
         Postcodify_Utility::print_message('다운로드: ' . basename(self::ENGLISH_URL));
@@ -215,7 +231,7 @@ class Postcodify_Indexer_Download
         
         // 파일 수가 맞는지 확인한다.
         
-        if ($downloaded_files < 47)
+        if ($downloaded_files < 48)
         {
             echo '[ERROR] 다운로드한 파일 수가 일치하지 않습니다.' . PHP_EOL;
             exit(2);
